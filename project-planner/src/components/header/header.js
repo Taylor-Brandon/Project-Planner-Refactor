e1a8537481834f95326b7d87d534aa225733fec2
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-07 13:46:09 -0400

    added header animation in css file

--- project-planner/src/components/header/header.js
+++ project-planner/src/components/header/header.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 export default function Headers(props) {
     return(
+        <div>
         <div className='header'>
             <h1 id='main-header' className='is-size-3'>Project Planner</h1>
+            </div>
             <div className='date is-size-5 has-text-white mb-4 ml-3'>
             <h2 id='current-date'>{props.today}</h2>
             <h3 id='current-time'>{props.time}</h3>
             </div>
-        </div>
+            </div>
     )
 }
