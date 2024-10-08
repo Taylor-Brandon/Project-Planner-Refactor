66e56b9067730ab53bc7c0ede44af497a249f09b
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-10-08 14:52:31 -0400

    ensured event saved after browser refresh

--- project-planner/src/components/card/card.js
+++ project-planner/src/components/card/card.js
@@ -3,85 +3,54 @@ import dayjs from 'dayjs';
 import CardForm from '../cardForm/cardForm';
 
 export default function Card(props) {
-        const current = dayjs();
-        const tomorrow = current.add(1, 'day').format('MMM D, YYYY');
-        const day2 = current.add(2, 'day').format('MMM D, YYYY');
-        const day3 = current.add(3, 'day').format('MMM D, YYYY');
-        const day4 = current.add(4, 'day').format('MMM D, YYYY');
-        const day5 = current.add(5, 'day').format('MMM D, YYYY');
-        const day6 = current.add(6, 'day').format('MMM D, YYYY');
+    const current = dayjs();
+    const tomorrow = current.add(1, 'day').format('MMM D, YYYY');
+    const day2 = current.add(2, 'day').format('MMM D, YYYY');
+    const day3 = current.add(3, 'day').format('MMM D, YYYY');
+    const day4 = current.add(4, 'day').format('MMM D, YYYY');
+    const day5 = current.add(5, 'day').format('MMM D, YYYY');
+    const day6 = current.add(6, 'day').format('MMM D, YYYY');
 
-        const timeSlots = [
-            {id: 1, dayJs: dayjs().hour(1).minute(0).second(0), realTime: '1:00am'},
-            {id: 2, dayJs: dayjs().hour(2).minute(0).second(0), realTime: '2:00am'},
-            {id: 3, dayJs: dayjs().hour(3).minute(0).second(0), realTime: '3:00am'},
-            {id: 4, dayJs: dayjs().hour(4).minute(0).second(0), realTime: '4:00am'},
-            {id: 5, dayJs: dayjs().hour(5).minute(0).second(0), realTime: '5:00am'},
-            {id: 6, dayJs: dayjs().hour(6).minute(0).second(0), realTime: '6:00am'},
-            {id: 7, dayJs: dayjs().hour(7).minute(0).second(0), realTime: '7:00am'},
-            {id: 8, dayJs: dayjs().hour(8).minute(0).second(0), realTime: '8:00am'},
-            {id: 9, dayJs: dayjs().hour(9).minute(0).second(0), realTime: '9:00am'},
-            {id: 10, dayJs: dayjs().hour(10).minute(0).second(0), realTime: '10:00am'},
-            {id: 11, dayJs: dayjs().hour(11).minute(0).second(0), realTime: '11:00am'},
-            {id: 12, dayJs: dayjs().hour(12).minute(0).second(0), realTime: '12:00pm'},
-            {id: 13, dayJs: dayjs().hour(13).minute(0).second(0), realTime: '1:00pm'},
-            {id: 14, dayJs: dayjs().hour(14).minute(0).second(0), realTime: '2:00pm'},
-            {id: 15, dayJs: dayjs().hour(15).minute(0).second(0), realTime: '3:00pm'},
-            {id: 16, dayJs: dayjs().hour(16).minute(0).second(0), realTime: '4:00pm'},
-            {id: 17, dayJs: dayjs().hour(17).minute(0).second(0), realTime: '5:00pm'},
-            {id: 18, dayJs: dayjs().hour(18).minute(0).second(0), realTime: '6:00pm'},
-            {id: 19, dayJs: dayjs().hour(19).minute(0).second(0), realTime: '7:00pm'},
-            {id: 20, dayJs: dayjs().hour(20).minute(0).second(0), realTime: '8:00pm'},
-            {id: 21, dayJs: dayjs().hour(21).minute(0).second(0), realTime: '9:00pm'},
-            {id: 22, dayJs: dayjs().hour(22).minute(0).second(0), realTime: '10:00pm'},
-            {id: 23, dayJs: dayjs().hour(23).minute(0).second(0), realTime: '11:00pm'},
-            {id: 24, dayJs: dayjs().hour(24).minute(0).second(0), realTime: '12:00am'},
-        ];
+    const getTimeDiff = (time) => {
+        if (time.isBefore(current, 'hour')) {
+            return 'has-background-danger-dark has-text-danger';
+        } else if (time.isSame(current, 'hour')) {
+            return 'has-background-info-dark has-text-info';
+        } else {
+            return 'has-background-primary-dark has-text-primary';
+        }
+    };
 
-        
-        const getTimeDiff = (time) => {
-            if (time.isBefore(current, 'hour')) {
-                console.log('Past: is-primary');
-                return 'has-background-danger-dark has-text-danger';
-            } else if (time.isSame(current, 'hour')) {
-                console.log('Present: has-text-success');
-                return 'has-background-info-dark has-text-info';
-            } else {
-                console.log('Future: has-text-danger');
-                return 'has-background-primary-dark has-text-primary';
-            }
-        };
-        
-    return(
+    return (
         <div style={{ overflowX: 'auto' }}>
-        <table className='table is-bordered is-hoverable'>
-            <thead>
-                <tr>
-                    <th>Time</th>
-                    <th>{props.today}</th>
-                    <th>{tomorrow}</th>
-                    <th>{day2}</th>
-                    <th>{day3}</th>
-                    <th>{day4}</th>
-                    <th>{day5}</th>
-                    <th>{day6}</th>
-                </tr>
-            </thead>
-            <tbody>
-            {timeSlots.map((slot) => (
+            <table className='table is-bordered is-hoverable'>
+                <thead>
+                    <tr>
+                        <th>Time</th>
+                        <th>{props.today}</th>
+                        <th>{tomorrow}</th>
+                        <th>{day2}</th>
+                        <th>{day3}</th>
+                        <th>{day4}</th>
+                        <th>{day5}</th>
+                        <th>{day6}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.timeSlots.map((slot) => (
                         <tr key={slot.id}>
                             <th className={`time-slot ${getTimeDiff(slot.dayJs)}`}>{slot.realTime}</th>
-                            <td><CardForm /></td>
-                            <td><CardForm /></td>
-                            <td><CardForm /></td>
-                            <td><CardForm /></td>
-                            <td><CardForm /></td>
-                            <td><CardForm /></td>
-                            <td><CardForm /></td>
+                            <td><CardForm timeSlot={slot} day={props.today} /></td>
+                            <td><CardForm timeSlot={slot} day={tomorrow} /></td>
+                            <td><CardForm timeSlot={slot} day={day2} /></td>
+                            <td><CardForm timeSlot={slot} day={day3} /></td>
+                            <td><CardForm timeSlot={slot} day={day4} /></td>
+                            <td><CardForm timeSlot={slot} day={day5} /></td>
+                            <td><CardForm timeSlot={slot} day={day6} /></td>
                         </tr>
                     ))}
-            </tbody>
-        </table>
+                </tbody>
+            </table>
         </div>
     );
 }
--- project-planner/src/components/cardForm/cardForm.js
+++ project-planner/src/components/cardForm/cardForm.js
@@ -1,40 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function CardForm () {
-    const [event, setEvent] = useState({eventName: ''});
+export default function CardForm (props) {
+    const [event, setEvent] = useState({ eventName: '', eventTime: '' });
 
+
+    useEffect(() => {
+        const savedEvent = JSON.parse(localStorage.getItem(`event-${props.timeSlot.id}`));
+        if (savedEvent) {
+            setEvent(savedEvent); 
+        }
+    }, [props.timeSlot.id]);
+
+    
     const handleFormChange = (e) => {
-        const {name, value} = e.target;
-
-        setEvent({
-            ...event,
+        const { name, value } = e.target;
+        setEvent((prevEvent) => ({
+            ...prevEvent,
             [name]: value,
-        });
+        }));
     };
 
+    
     const handleSubmit = (e) => {
         e.preventDefault();
-        localStorage.setItem("event", JSON.stringify(event));
-        console.log(event);
-    }
 
+        
+        localStorage.setItem(
+            `event-${props.timeSlot.id}`, 
+            JSON.stringify({ eventName: event.eventName, eventTime: props.timeSlot.time })
+        );
+
+        console.log("Saved Event:", { eventName: event.eventName, eventTime: props.timeSlot.time });
+    };
+
+    
     const handleDelete = (e) => {
         e.preventDefault();
-        localStorage.removeItem("event");
-        setEvent({eventName: ''});
-        alert('Done!');
-    }
 
-    const savedEvent = JSON.parse(localStorage.getItem("event"));
+       
+        localStorage.removeItem(`event-${props.timeSlot.id}`);
 
+        setEvent({ eventName: '', eventTime: '' });
+        alert('Event Deleted!');
+    };
 
     return (
         <div className='field'>
             <div className='control'>
                 <div className='form-container'>
-                <input id='input' onChange={handleFormChange} name='eventName' value={event.eventName} className='input' type='text'></input>
-                <button id='add' onClick={handleSubmit} className='button is-small'>+</button>
-                <button id='delete' onClick={handleDelete} className='button is-small'>x</button>
+                    <input 
+                        id='input' 
+                        onChange={handleFormChange} 
+                        name='eventName' 
+                        value={event.eventName} 
+                        className='input' 
+                        type='text' 
+                    />
+                    
+                  
+                    <button id='add' onClick={handleSubmit} className='button is-small'>+</button>
+                    
+                    
+                    <button id='delete' onClick={handleDelete} className='button is-small'>x</button>
                 </div>
             </div>
         </div>
